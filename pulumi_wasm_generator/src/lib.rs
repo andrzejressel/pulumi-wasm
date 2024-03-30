9c6e6bde3a2441c9760cf032dad97c4ee47c4d05
Andrzej Ressel <github@andrzejressel.pl>
2024-03-30 21:04:49 +0100

    Code generation

--- pulumi_wasm_generator/src/lib.rs
+++ pulumi_wasm_generator/src/lib.rs
@@ -15,19 +15,14 @@ pub fn generate_rust_library(schema_json: &Path, result_path: &Path) -> Result<(
     let package_json: schema::Package = serde_json::from_reader(reader)?;
     let package = schema::to_model(&package_json)?;
 
-    fs::create_dir_all(result_path.join("wit").join("deps").join("pulumi-wasm"))?;
+    fs::create_dir_all(result_path.join("wit").join("deps"))?;
     fs::create_dir_all(result_path.join("src"))?;
 
     let mut wit_file = File::create(result_path.join("wit").join("world.wit"))?;
     wit_file.write_all(output::wit::generate_wit(&package)?.as_ref())?;
 
-    let mut deps_wit_file = File::create(
-        result_path
-            .join("wit")
-            .join("deps")
-            .join("pulumi-wasm")
-            .join("pulumi-wasm.wit"),
-    )?;
+    let mut deps_wit_file =
+        File::create(result_path.join("wit").join("deps").join("pulumi-wasm.wit"))?;
     deps_wit_file.write_all(output::wit::get_dependencies().as_ref())?;
 
     let mut cargo_file = File::create(result_path.join("Cargo.toml"))?;
