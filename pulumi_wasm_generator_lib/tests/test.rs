2159ef0e062184ff7dcf2340d31258a7a791348c
Andrzej Ressel <github@andrzejressel.pl>
2024-07-14 00:43:11 +0200

    Show cargo timings

--- pulumi_wasm_generator_lib/tests/test.rs
+++ pulumi_wasm_generator_lib/tests/test.rs
@@ -142,7 +142,7 @@ fn run_real_provider_test(provider_name: &str) -> Result<()> {
             "build",
             "-p",
             format!("pulumi_wasm_{provider_name}_provider").as_str(),
-            "--timings"
+            "--timings",
         ])
         .current_dir(root)
         .assert()
@@ -153,7 +153,7 @@ fn run_real_provider_test(provider_name: &str) -> Result<()> {
             "build",
             "-p",
             format!("pulumi_wasm_{provider_name}").as_str(),
-            "--timings"
+            "--timings",
         ])
         .current_dir(root)
         .assert()
