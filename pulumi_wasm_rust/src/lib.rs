a3cba15ed6e10b341284646ff011da19b7dbab09
Andrzej Ressel <github@andrzejressel.pl>
2024-07-15 00:19:03 +0200

    Reworking directory structure

--- pulumi_wasm_rust/src/lib.rs
+++ pulumi_wasm_rust/src/lib.rs
@@ -1,18 +1,7 @@
 pub use pulumi_wasm_rust_macro::pulumi_main;
 mod output;
 pub use output::Output;
-use pulumi_wasm_wit::bindings as bindings;
-//
-// #[allow(clippy::all)]
-// #[allow(dead_code)]
-// #[allow(unused_variables)]
-// #[allow(unused_unsafe)]
-// mod bindings {
-//     wit_bindgen::generate!({
-//         // the name of the world in the `*.wit` input file
-//         world: "pulumi-wasm-rust"
-//     });
-// }
+use pulumi_wasm_wit::bindings;
 
 pub mod runner;
 
