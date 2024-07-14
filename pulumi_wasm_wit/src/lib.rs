560d61795c0d416008904eb096aed3a52eb583ea
Andrzej Ressel <github@andrzejressel.pl>
2024-07-15 00:12:29 +0200

    Reworking directory structure

---
+++ pulumi_wasm_wit/src/lib.rs
@@ -0,0 +1,23 @@
+#[allow(clippy::all)]
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[allow(unused_unsafe)]
+#[cfg(feature = "pulumi-wasm-rust")]
+pub mod bindings {
+    wit_bindgen::generate!({
+        // the name of the world in the `*.wit` input file
+        world: "pulumi-wasm-rust",
+    });
+}
+
+#[allow(clippy::all)]
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[allow(unused_unsafe)]
+#[cfg(feature = "logger")]
+pub mod bindings_logger {
+    wit_bindgen::generate!({
+        // the name of the world in the `*.wit` input file
+        world: "logger",
+    });
+}
