bbece2ee8f97a31a0eebfde2d164b3acd095446f
Andrzej Ressel <github@andrzejressel.pl>
2024-08-26 00:22:43 +0200

    Release and debug builds

--- providers/pulumi_wasm_provider_cloudflare_rust/src/lib.rs
+++ providers/pulumi_wasm_provider_cloudflare_rust/src/lib.rs
@@ -1,11 +1,11 @@
 use pulumi_wasm_rust::Output;
 use pulumi_wasm_wit::client_bindings::component::pulumi_wasm::output_interface::Output as WitOutput;
 #[allow(clippy::doc_lazy_continuation, clippy::tabs_in_doc_comments)]
-mod types;
-pub use types::*;
-#[allow(clippy::doc_lazy_continuation, clippy::tabs_in_doc_comments)]
 mod resource;
 pub use resource::*;
+#[allow(clippy::doc_lazy_continuation, clippy::tabs_in_doc_comments)]
+mod types;
+pub use types::*;
 
 mod bindings {
     wit_bindgen::generate!({
