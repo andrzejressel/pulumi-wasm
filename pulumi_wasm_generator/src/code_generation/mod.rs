99e2f2351df5970f45a0c5b0c4b987b3f73bb027
Andrzej Ressel <github@andrzejressel.pl>
2024-12-30 02:07:10 +0100

    Add anyhow to YAML example converter

--- pulumi_wasm_generator/src/code_generation/mod.rs
+++ pulumi_wasm_generator/src/code_generation/mod.rs
@@ -1,9 +1,7 @@
 use crate::code_generation::rust_generation::generate_code;
 use crate::code_generation::yaml::model::yaml_to_model;
 use crate::code_generation::yaml::yaml_model::YamlFile;
-use anyhow::anyhow;
 use anyhow::Context;
-use std::panic;
 pub(crate) mod rust_generation;
 mod tests;
 pub(crate) mod yaml;
@@ -14,8 +12,7 @@ pub fn generate_code_from_string(
 ) -> anyhow::Result<String> {
     let yaml_file =
         YamlFile::from_yaml(yaml.as_str()).context(format!("Failed to parse YAML: {}", yaml))?;
-    let example = panic::catch_unwind(|| yaml_to_model(yaml_file, package.name.clone(), package))
-        .map_err(|_| anyhow!("Failed to convert YAML to model"))
-        .context(format!("Failed to convert YAML {} to model", yaml))?;
+    let example = yaml_to_model(yaml_file, package.name.clone(), package)
+        .with_context(|| format!("Failed to convert YAML {} to model", yaml))?;
     generate_code(example)
 }
