d7fa0b7434df33ed7b86e6d2a3801ae280d8ab20
Andrzej Ressel <github@andrzejressel.pl>
2025-01-05 00:11:26 +0100

    Update

--- pulumi_wasm_generator/tests/output/mini-awsnative/src/generated/types/region.rs
+++ pulumi_wasm_generator/tests/output/mini-awsnative/src/generated/types/region.rs
@@ -3,77 +3,77 @@
 pub enum Region {
     /// Africa (Cape Town)
     #[serde(rename = "af-south-1")]
-    AfSouth1,
+    AFSouth1,
     /// Asia Pacific (Hong Kong)
     #[serde(rename = "ap-east-1")]
-    ApEast1,
+    APEast1,
     /// Asia Pacific (Tokyo)
     #[serde(rename = "ap-northeast-1")]
-    ApNortheast1,
+    APNortheast1,
     /// Asia Pacific (Seoul)
     #[serde(rename = "ap-northeast-2")]
-    ApNortheast2,
+    APNortheast2,
     /// Asia Pacific (Osaka)
     #[serde(rename = "ap-northeast-3")]
-    ApNortheast3,
+    APNortheast3,
     /// Asia Pacific (Mumbai)
     #[serde(rename = "ap-south-1")]
-    ApSouth1,
+    APSouth1,
     /// Asia Pacific (Singapore)
     #[serde(rename = "ap-southeast-1")]
-    ApSoutheast1,
+    APSoutheast1,
     /// Asia Pacific (Sydney)
     #[serde(rename = "ap-southeast-2")]
-    ApSoutheast2,
+    APSoutheast2,
     /// Canada (Central)
     #[serde(rename = "ca-central-1")]
-    CaCentral,
+    CACentral,
     /// China (Beijing)
     #[serde(rename = "cn-north-1")]
-    CnNorth1,
+    CNNorth1,
     /// China (Ningxia)
     #[serde(rename = "cn-northwest-1")]
-    CnNorthwest1,
+    CNNorthwest1,
     /// Europe (Frankfurt)
     #[serde(rename = "eu-central-1")]
-    EuCentral1,
+    EUCentral1,
     /// Europe (Stockholm)
     #[serde(rename = "eu-north-1")]
-    EuNorth1,
+    EUNorth1,
     /// Europe (Ireland)
     #[serde(rename = "eu-west-1")]
-    EuWest1,
+    EUWest1,
     /// Europe (London)
     #[serde(rename = "eu-west-2")]
-    EuWest2,
+    EUWest2,
     /// Europe (Paris)
     #[serde(rename = "eu-west-3")]
-    EuWest3,
+    EUWest3,
     /// Europe (Milan)
     #[serde(rename = "eu-south-1")]
-    EuSouth1,
+    EUSouth1,
     /// Middle East (Bahrain)
     #[serde(rename = "me-south-1")]
-    MeSouth1,
+    MESouth1,
     /// South America (São Paulo)
     #[serde(rename = "sa-east-1")]
-    SaEast1,
+    SAEast1,
     /// AWS GovCloud (US-East)
     #[serde(rename = "us-gov-east-1")]
-    UsGovEast1,
+    USGovEast1,
     /// AWS GovCloud (US-West)
     #[serde(rename = "us-gov-west-1")]
-    UsGovWest1,
+    USGovWest1,
     /// US East (N. Virginia)
     #[serde(rename = "us-east-1")]
-    UsEast1,
+    USEast1,
     /// US East (Ohio)
     #[serde(rename = "us-east-2")]
-    UsEast2,
+    USEast2,
     /// US West (N. California)
     #[serde(rename = "us-west-1")]
-    UsWest1,
+    USWest1,
     /// US West (Oregon)
     #[serde(rename = "us-west-2")]
-    UsWest2,
+    USWest2,
 }
