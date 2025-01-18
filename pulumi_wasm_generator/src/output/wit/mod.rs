3d8528ca428d7e2a2a2f36cca7f7c82814104c0b
Andrzej Ressel <github@andrzejressel.pl>
2025-01-19 00:32:29 +0100

    Return plugins

--- pulumi_wasm_generator/src/output/wit/mod.rs
+++ pulumi_wasm_generator/src/output/wit/mod.rs
@@ -3,7 +3,7 @@ use handlebars::Handlebars;
 
 static DEPENDENCIES: &str = include_str!("dependencies.handlebars");
 
-pub(crate) fn get_dependencies(provider_name: &String) -> anyhow::Result<String> {
+pub(crate) fn get_dependencies(provider_name: &str) -> anyhow::Result<String> {
     let mut data = std::collections::BTreeMap::new();
     data.insert("pulumi_wasm_version", get_main_version());
     data.insert("provider_name", provider_name);
