a5f30b9b3de8280f8f93b061c448a21fff20f6cd
Andrzej Ressel <github@andrzejressel.pl>
2025-01-22 23:42:52 +0100

    Update

--- examples/common/src/lib.rs
+++ examples/common/src/lib.rs
@@ -1,8 +1,8 @@
+use anyhow::anyhow;
 use assert_cmd::prelude::*;
 use serde_json::Value;
 use std::process::Command;
 use std::str;
-use anyhow::anyhow;
 
 pub struct Stack {
     value: Value,
@@ -30,7 +30,10 @@ pub fn get_string(pointer: &str) -> &str {
     pointer
 }
 
-pub fn init_stack(stack_name: &str, github_token_env_vars: Vec<(String, String)>) -> Result<(), anyhow::Error> {
+pub fn init_stack(
+    stack_name: &str,
+    github_token_env_vars: Vec<(String, String)>,
+) -> Result<(), anyhow::Error> {
     Command::new("pulumi")
         .args(["stack", "init", stack_name])
         .env("PULUMI_CONFIG_PASSPHRASE", " ")
@@ -62,7 +65,7 @@ pub fn up_stack(github_token_env_vars: Vec<(String, String)>) -> Result<(), anyh
 
 pub fn export_stack() -> Result<Stack, anyhow::Error> {
     let binding = Command::new("pulumi")
-        .args(["stack", "export"])
+        .args(["stack", "output", "--json"])
         .current_dir(".")
         .env("PULUMI_CONFIG_PASSPHRASE", " ")
         .assert()
