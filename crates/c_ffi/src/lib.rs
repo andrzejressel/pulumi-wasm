2b23508df6bf703aa6b1908ed84892f6290559f7
​Andrzej Ressel <github@andrzejressel.pl>
2025-02-11 01:19:35 +0100

    rust_adapter_native_simple (#786)

--- crates/c_ffi/src/lib.rs
+++ crates/c_ffi/src/lib.rs
@@ -1,33 +1,28 @@
-mod native_pulumi_connector;
-
-use anyhow::Context;
-use pulumi_gestalt_core::{
-    Engine, FieldName, ForeignFunctionToInvoke, FunctionName, OutputId, PulumiServiceImpl,
-};
-use serde_json::Value;
+use pulumi_gestalt_core::{FieldName, OutputId};
+use pulumi_gestalt_rust_adapter_native_simple as simple;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::ffi::{c_char, c_void, CStr, CString};
-use std::ops::Deref;
-use std::rc::Rc;
-use uuid::Uuid;
+use std::rc::{Rc, Weak};
 
 pub struct CustomOutputId {
-    output_id: OutputId,
-    engine: Rc<RefCell<Engine>>,
+    native: simple::CustomOutputId,
+    engine: Weak<RefCell<InnerPulumiEngine>>,
 }
 
 pub struct CustomRegisterOutputId {
-    output_id: OutputId,
-    engine: Rc<RefCell<Engine>>,
+    native: simple::CustomRegisterOutputId,
+    engine: Weak<RefCell<InnerPulumiEngine>>,
+}
+
+pub struct InnerPulumiEngine {
+    engine: simple::PulumiEngine,
+    outputs: Vec<*mut CustomOutputId>,
+    context: *const c_void,
 }
 
 pub struct PulumiEngine {
-    engine: Rc<RefCell<Engine>>,
-    outputs: Vec<*mut CustomOutputId>,
-    functions: HashMap<FunctionName, Box<dyn Fn(Value) -> Value>>,
-    in_preview: bool,
-    context: *const c_void,
+    inner: Rc<RefCell<InnerPulumiEngine>>,
 }
 
 #[repr(C)]
@@ -69,18 +64,14 @@ type MappingFunction = extern "C" fn(*const c_void, *const c_void, *const c_char
 
 #[no_mangle]
 extern "C" fn create_engine(context: *const c_void) -> *mut PulumiEngine {
-    let engine = Rc::new(RefCell::new(get_engine()));
-    let in_preview = match std::env::var("PULUMI_DRY_RUN") {
-        Ok(preview) if preview == "true" => true,
-        Ok(preview) if preview == "false" => false,
-        _ => false,
-    };
-    let t = PulumiEngine {
+    let engine = simple::PulumiEngine::create_engine();
+    let t = InnerPulumiEngine {
         engine,
         outputs: Vec::new(),
-        functions: HashMap::new(),
-        in_preview,
         context,
+    };
+    let t = PulumiEngine {
+        inner: Rc::new(RefCell::new(t)),
     };
     Box::into_raw(Box::new(t))
 }
@@ -89,7 +80,7 @@ extern "C" fn create_engine(context: *const c_void) -> *mut PulumiEngine {
 extern "C" fn free_engine(t: *mut PulumiEngine) {
     unsafe {
         let b = Box::from_raw(t);
-        for output in b.outputs.iter() {
+        for output in b.inner.borrow_mut().outputs.iter() {
             let _ = Box::from_raw(*output);
         }
     }
@@ -106,18 +97,15 @@ extern "C" fn create_output(
         .unwrap()
         .to_string();
     let pulumi_engine = unsafe { &mut *pulumi_engine };
-    let value = serde_json::from_str(&value).unwrap();
-    let output_id = pulumi_engine
-        .engine
-        .deref()
-        .borrow_mut()
-        .create_done_node(value, secret);
+    let inner = &pulumi_engine.inner;
+    let mut inner_engine = pulumi_engine.inner.borrow_mut();
+    let output_id = inner_engine.engine.create_output(value, secret);
     let output = CustomOutputId {
-        output_id,
-        engine: Rc::clone(&pulumi_engine.engine),
+        native: output_id,
+        engine: Rc::downgrade(inner),
     };
     let raw = Box::into_raw(Box::new(output));
-    pulumi_engine.outputs.push(raw);
+    inner_engine.outputs.push(raw);
     raw
 }
 
@@ -128,46 +116,13 @@ extern "C" fn add_export(value: *const CustomOutputId, name: *const c_char) {
         .unwrap()
         .to_string();
     let value = unsafe { &*value };
-    let pulumi_engine = &value.engine;
-    let output_id = value.output_id;
-    pulumi_engine
-        .deref()
-        .borrow_mut()
-        .add_output(name.into(), output_id);
+    value.native.add_export(name);
 }
 
 #[no_mangle]
 extern "C" fn finish(pulumi_engine: *mut PulumiEngine) {
     let pulumi_engine = unsafe { &mut *pulumi_engine };
-    finish_loop(pulumi_engine, HashMap::new());
-}
-
-fn finish_loop(pulumi_engine: &mut PulumiEngine, native_function_result: HashMap<OutputId, Value>) {
-    let mut mut_borrow = pulumi_engine.engine.deref().borrow_mut();
-    let result = mut_borrow.run(native_function_result);
-    drop(mut_borrow);
-
-    match result {
-        None => (),
-        Some(functions_to_invoke) => {
-            let mut results = HashMap::new();
-
-            for ForeignFunctionToInvoke {
-                output_id,
-                function_name,
-                value,
-            } in functions_to_invoke.iter()
-            {
-                let function = pulumi_engine.functions.get(function_name).unwrap();
-
-                let result = function(value.clone());
-
-                results.insert(*output_id, result);
-            }
-
-            finish_loop(pulumi_engine, results);
-        }
-    }
+    pulumi_engine.inner.borrow_mut().engine.finish();
 }
 
 #[no_mangle]
@@ -179,45 +134,28 @@ extern "C" fn pulumi_map(
 ) -> *mut CustomOutputId {
     let output = unsafe { &*output };
     let engine = unsafe { &mut *pulumi_engine };
-    let engine_context = engine.context;
+    let mut inner_engine = engine.inner.borrow_mut();
+    let context = inner_engine.context;
 
-    // let pulumi_engine = &output.engine;
-    let output_id = output.output_id;
-    // let engine = Rc::clone(pulumi_engine);
-    let function_uuid = Uuid::new_v4();
-    let function_name: FunctionName = function_uuid.to_string().into();
-
-    let output = engine
-        .engine
-        .borrow_mut()
-        .create_native_function_node(function_name.clone(), output_id);
-    let output = CustomOutputId {
-        output_id: output,
-        engine: Rc::clone(&engine.engine),
+    let f = move |value: String| {
+        let c_string = CString::new(value).unwrap();
+        let str = function(context, function_context, c_string.as_ptr());
+        let result = unsafe { CStr::from_ptr(str) }.to_str().unwrap();
+        let v = result.to_owned();
+        unsafe {
+            libc::free(str as *mut c_void);
+        }
+        v
     };
 
-    engine.functions.insert(
-        function_name,
-        Box::new(move |value| {
-            let value_string = serde_json::to_string(&value).unwrap();
-            let c_string = CString::new(value_string).unwrap();
+    let second_output = inner_engine.engine.map(&output.native, Box::new(f));
 
-            let str = function(engine_context, function_context, c_string.as_ptr());
-
-            let result = unsafe { CStr::from_ptr(str) }.to_str().unwrap();
-            let v: Value = serde_json::from_str(result)
-                .with_context(|| format!("Failed to parse JSON: {}", result))
-                .unwrap();
-
-            unsafe {
-                libc::free(str as *mut c_void);
-            }
-            v
-        }),
-    );
-
+    let output = CustomOutputId {
+        native: second_output,
+        engine: Rc::downgrade(&engine.inner),
+    };
     let raw = Box::into_raw(Box::new(output));
-    engine.outputs.push(raw);
+    inner_engine.outputs.push(raw);
     raw
 }
 
@@ -226,24 +164,23 @@ extern "C" fn pulumi_get_output(
     custom_register_output_id: *mut CustomRegisterOutputId,
     field_name: *const c_char,
 ) -> *mut CustomOutputId {
-    let pulumi_engine = unsafe { &(*custom_register_output_id).engine.clone() };
-
-    let output_id = unsafe { &(*custom_register_output_id).output_id.clone() };
-
     let field_name = unsafe { CStr::from_ptr(field_name) }
         .to_str()
         .unwrap()
         .to_string();
+    let custom_register_output_id = unsafe { &*custom_register_output_id };
+    let output = custom_register_output_id.native.get_output(field_name);
 
-    let output = pulumi_engine
-        .borrow_mut()
-        .create_extract_field(field_name.into(), *output_id);
+    let binding = custom_register_output_id.engine.upgrade().unwrap();
+    let mut engine = binding.borrow_mut();
 
     let output = CustomOutputId {
-        output_id: output,
-        engine: Rc::clone(pulumi_engine),
+        native: output,
+        engine: custom_register_output_id.engine.clone(),
     };
-    Box::into_raw(Box::new(output))
+    let raw = Box::into_raw(Box::new(output));
+    engine.outputs.push(raw);
+    raw
 }
 
 #[no_mangle]
@@ -252,66 +189,51 @@ extern "C" fn pulumi_register_resource(
     request: *const RegisterResourceRequest,
 ) -> *mut CustomRegisterOutputId {
     let pulumi_engine = unsafe { &mut *pulumi_engine };
+    let request = unsafe { &*request };
 
-    let type_ = unsafe { CStr::from_ptr((*request).type_) }
+    let type_ = unsafe { CStr::from_ptr(request.type_) }
         .to_str()
         .unwrap()
         .to_owned();
 
-    let name = unsafe { CStr::from_ptr((*request).name) }
+    let name = unsafe { CStr::from_ptr(request.name) }
         .to_str()
         .unwrap()
         .to_owned();
 
-    let version = unsafe { CStr::from_ptr((*request).version) }
+    let version = unsafe { CStr::from_ptr(request.version) }
         .to_str()
         .unwrap()
         .to_owned();
 
-    let mut inputs: HashMap<FieldName, OutputId> = HashMap::new();
+    let mut objects: HashMap<FieldName, OutputId> = HashMap::new();
 
     unsafe {
-        let request = &*request;
         std::slice::from_raw_parts(request.object, request.object_len)
             .iter()
             .for_each(|field| {
                 let name = CStr::from_ptr(field.name).to_str().unwrap().to_owned();
-                let output = (*field.value).output_id;
+                let output = &(*field.value).native.get_id().clone();
 
-                inputs.insert(name.into(), output);
+                objects.insert(name.into(), *output);
             });
     }
 
-    let output_id = pulumi_engine
-        .engine
-        .deref()
-        .borrow_mut()
-        .create_register_resource_node(type_, name, inputs, version);
-
-    let register_output_id = CustomRegisterOutputId {
-        output_id,
-        engine: Rc::clone(&pulumi_engine.engine),
+    let inner = &pulumi_engine.inner;
+    let inner_engine = pulumi_engine.inner.borrow_mut();
+    let request = simple::RegisterResourceRequest {
+        type_,
+        name,
+        objects,
+        version,
     };
-    Box::into_raw(Box::new(register_output_id))
-}
+    let output_id = inner_engine.engine.pulumi_register_resource(request);
 
-fn get_engine() -> Engine {
-    let pulumi_engine_url = std::env::var("PULUMI_ENGINE").unwrap();
-    let pulumi_monitor_url = std::env::var("PULUMI_MONITOR").unwrap();
-    let pulumi_stack = std::env::var("PULUMI_STACK").unwrap();
-    let pulumi_project = std::env::var("PULUMI_PROJECT").unwrap();
-    let in_preview = match std::env::var("PULUMI_DRY_RUN") {
-        Ok(preview) if preview == "true" => true,
-        Ok(preview) if preview == "false" => false,
-        _ => false,
+    let output = CustomRegisterOutputId {
+        native: output_id,
+        engine: Rc::downgrade(inner),
     };
 
-    let native_pulumi_connector = native_pulumi_connector::NativePulumiConnector::new(
-        pulumi_monitor_url,
-        pulumi_engine_url,
-        pulumi_project,
-        pulumi_stack,
-    );
-
-    Engine::new(PulumiServiceImpl::new(native_pulumi_connector, in_preview))
+    // inner_engine.outputs.push(raw); //FIXME
+    Box::into_raw(Box::new(output))
 }
---
+++ crates/rust_adapter_native_simple/src/lib.rs
@@ -0,0 +1,210 @@
+mod native_pulumi_connector;
+
+use pulumi_gestalt_core::{
+    Engine, FieldName, ForeignFunctionToInvoke, FunctionName, OutputId, PulumiServiceImpl,
+};
+use serde_json::Value;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::ops::Deref;
+use std::rc::Rc;
+use uuid::Uuid;
+
+pub struct CustomOutputId {
+    output_id: OutputId,
+    engine: Rc<RefCell<Engine>>,
+}
+
+impl CustomOutputId {
+    pub fn get_id(&self) -> &OutputId {
+        &self.output_id
+    }
+}
+
+pub struct CustomRegisterOutputId {
+    output_id: OutputId,
+    engine: Rc<RefCell<Engine>>,
+}
+
+pub struct PulumiEngine {
+    engine: Rc<RefCell<Engine>>,
+    functions: HashMap<FunctionName, Box<dyn Fn(String) -> String>>,
+    in_preview: bool,
+}
+
+pub struct ObjectField {
+    name: String,
+    value: CustomOutputId,
+}
+
+pub struct ResultField {
+    name: String,
+}
+
+pub struct RegisterResourceResultField {
+    name: String,
+    output: CustomOutputId,
+}
+
+pub struct RegisterResourceRequest {
+    pub type_: String,
+    pub name: String,
+    pub version: String,
+    pub objects: HashMap<FieldName, OutputId>,
+}
+
+impl PulumiEngine {
+    pub fn create_engine() -> PulumiEngine {
+        let engine = Rc::new(RefCell::new(get_engine()));
+        let in_preview = match std::env::var("PULUMI_DRY_RUN") {
+            Ok(preview) if preview == "true" => true,
+            Ok(preview) if preview == "false" => false,
+            _ => false,
+        };
+        PulumiEngine {
+            engine,
+            functions: HashMap::new(),
+            in_preview,
+        }
+    }
+
+    pub fn create_output(&self, value: String, secret: bool) -> CustomOutputId {
+        let value = serde_json::from_str(&value).unwrap();
+        let output_id = self
+            .engine
+            .deref()
+            .borrow_mut()
+            .create_done_node(value, secret);
+        CustomOutputId {
+            output_id,
+            engine: Rc::clone(&self.engine),
+        }
+    }
+
+    pub fn pulumi_register_resource(
+        &self,
+        request: RegisterResourceRequest,
+    ) -> CustomRegisterOutputId {
+        let type_ = request.type_;
+        let name = request.name;
+        let version = request.version;
+
+        let output_id = self
+            .engine
+            .deref()
+            .borrow_mut()
+            .create_register_resource_node(type_, name, request.objects, version);
+
+        CustomRegisterOutputId {
+            output_id,
+            engine: Rc::clone(&self.engine),
+        }
+    }
+
+    pub fn map(
+        &mut self,
+        output: &CustomOutputId,
+        function: Box<dyn Fn(String) -> String>,
+    ) -> CustomOutputId {
+        // let pulumi_engine = &output.engine;
+        let output_id = output.output_id;
+        // let engine = Rc::clone(pulumi_engine);
+        let function_uuid = Uuid::new_v4();
+        let function_name: FunctionName = function_uuid.to_string().into();
+
+        let output = self
+            .engine
+            .borrow_mut()
+            .create_native_function_node(function_name.clone(), output_id);
+        let output = CustomOutputId {
+            output_id: output,
+            engine: Rc::clone(&self.engine),
+        };
+
+        self.functions.insert(function_name, function);
+
+        output
+    }
+
+    pub fn finish(&self) {
+        self.finish_loop(HashMap::new());
+    }
+
+    fn finish_loop(&self, mut native_function_result: HashMap<OutputId, Value>) {
+        let mut mut_borrow = self.engine.deref().borrow_mut();
+        loop {
+            let result = mut_borrow.run(native_function_result);
+
+            match result {
+                None => break,
+                Some(functions_to_invoke) => {
+                    native_function_result = HashMap::new();
+
+                    for ForeignFunctionToInvoke {
+                        output_id,
+                        function_name,
+                        value,
+                    } in functions_to_invoke.iter()
+                    {
+                        let function = self.functions.get(function_name).unwrap();
+                        let s = value.to_string();
+
+                        let result = function(s);
+
+                        let result = serde_json::from_str(&result).unwrap();
+
+                        native_function_result.insert(*output_id, result);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl CustomOutputId {
+    pub fn add_export(&self, name: String) {
+        let pulumi_engine = &self.engine;
+        let output_id = self.output_id;
+        pulumi_engine
+            .deref()
+            .borrow_mut()
+            .add_output(name.into(), output_id);
+    }
+}
+
+impl CustomRegisterOutputId {
+    pub fn get_output(&self, field_name: String) -> CustomOutputId {
+        let pulumi_engine = &self.engine;
+        let output_id = &self.output_id;
+
+        let output = pulumi_engine
+            .borrow_mut()
+            .create_extract_field(field_name.into(), *output_id);
+
+        CustomOutputId {
+            output_id: output,
+            engine: Rc::clone(pulumi_engine),
+        }
+    }
+}
+
+fn get_engine() -> Engine {
+    let pulumi_engine_url = std::env::var("PULUMI_ENGINE").unwrap();
+    let pulumi_monitor_url = std::env::var("PULUMI_MONITOR").unwrap();
+    let pulumi_stack = std::env::var("PULUMI_STACK").unwrap();
+    let pulumi_project = std::env::var("PULUMI_PROJECT").unwrap();
+    let in_preview = match std::env::var("PULUMI_DRY_RUN") {
+        Ok(preview) if preview == "true" => true,
+        Ok(preview) if preview == "false" => false,
+        _ => false,
+    };
+
+    let native_pulumi_connector = native_pulumi_connector::NativePulumiConnector::new(
+        pulumi_monitor_url,
+        pulumi_engine_url,
+        pulumi_project,
+        pulumi_stack,
+    );
+
+    Engine::new(PulumiServiceImpl::new(native_pulumi_connector, in_preview))
+}
