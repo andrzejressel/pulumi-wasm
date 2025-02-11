06ff97c62171529b2c6c50ce2cb037d7af646894
Andrzej Ressel <github@andrzejressel.pl>
2025-02-11 01:58:29 +0100

    Update

--- crates/rust_adapter_native/src/lib.rs
+++ crates/rust_adapter_native/src/lib.rs
@@ -1,5 +1,6 @@
 use std::marker::PhantomData;
-use pulumi_gestalt_rust_adapter::GestaltOutput;
+use serde::Serialize;
+use pulumi_gestalt_rust_adapter::{GestaltCompositeOutput, GestaltContext, GestaltOutput, InvokeResourceRequest, RegisterResourceRequest};
 use pulumi_gestalt_rust_adapter_native_simple as simple;
 
 struct NativeOutput<T> {
@@ -20,11 +21,66 @@ struct NativeContext {
     inner: simple::PulumiEngine
 }
 
+struct NativeCompositeOutput {
+    inner: simple::CustomRegisterOutputId
+}
+
+impl GestaltCompositeOutput for NativeCompositeOutput {
+    type Output<T> = NativeOutput<T>;
+
+    fn get_field<T>(&self, key: &str) -> Self::Output<T> {
+        let res = self.inner.get_output(key.to_string());
+        NativeOutput {
+            inner: res,
+            tpe: PhantomData
+        }
+    }
+}
+
 impl NativeContext {
     fn new() -> NativeContext {
         NativeContext {
             inner: simple::PulumiEngine::create_engine()
         }
+    }
+}
+
+impl GestaltContext for NativeContext {
+    type Output<T> = NativeOutput<T>;
+    type CompositeOutput = ();
+    type OutputId = ();
+
+    fn new_output<T: Serialize>(&self, value: &T) -> Self::Output<T> {
+        let json = serde_json::to_string(value).unwrap();
+        NativeOutput {
+            inner: self.inner.create_output(json, false),
+            tpe: PhantomData
+        }
+    }
+
+    fn new_secret<T: Serialize>(&self, value: &T) -> Self::Output<T> {
+        let json = serde_json::to_string(value).unwrap();
+        NativeOutput {
+            inner: self.inner.create_output(json, true),
+            tpe: PhantomData
+        }
+    }
+
+    fn register_resource(&self, request: RegisterResourceRequest<Self::OutputId>) -> Self::CompositeOutput {
+
+        let result = self.inner.pulumi_register_resource(simple::RegisterResourceRequest {
+            type_: request.type_,
+            name: request.name,
+            version: request.version,
+            objects: request.object.iter().map(|k| {
+                (k.name.clone(), k.value.inner)
+            }).collect()
+        });
+
+    }
+
+    fn invoke_resource(&self, request: InvokeResourceRequest<Self::OutputId>) -> Self::CompositeOutput {
+        todo!()
     }
 }
 
@@ -38,6 +94,21 @@ impl <T> GestaltOutput<T> for NativeOutput<T> {
         T: serde::de::DeserializeOwned,
         B: serde::ser::Serialize
     {
+
+        let function = move |v: String| {
+            let v: T = serde_json::from_str(&v).unwrap();
+            let v = f(v);
+            serde_json::to_string(&v).unwrap()
+        };
+
+        let res = self.inner.map(function);
+
+        // let res = &self.inner.map(|v| {
+        //     let v: T = serde_json::from_str(&v).unwrap();
+        //     let v = f(v);
+        //     serde_json::to_string(&v).unwrap()
+        // });
+
         todo!()
     }
 
