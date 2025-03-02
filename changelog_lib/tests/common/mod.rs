f79c3265100fa3efb3e3312382ca274cd10eff1c
​Andrzej Ressel <github@andrzejressel.pl>
2025-03-02 18:25:02 +0100

    Remove empty unreleased section from changelog (#864)

--- changelog_lib/tests/common/mod.rs
+++ changelog_lib/tests/common/mod.rs
@@ -5,103 +5,11 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use tempfile::{tempdir, TempDir};
 
-fn replace_files() -> bool {
+pub(crate) fn replace_files() -> bool {
     false
 }
 
-const INITIAL_COMMIT_ID: &str = "145c01c05387eaa60cf2bfa61a2e81b11c6ef57d";
-
-#[test]
-fn generate_changelog_test() -> Result<()> {
-    let repository = create_repository().context("Failed to create repository")?;
-
-    let options = changelog_lib::Options {
-        repository_path: repository.dir.path(),
-        start_commit_id: INITIAL_COMMIT_ID,
-        repository: "andrzejressel/pulumi-gestalt",
-        changelog_dir: "tests/example/.changelog",
-    };
-
-    let result = changelog_lib::generate_changelog(&options)?;
-
-    compare_with_file(&result, Path::new("tests/example/expected.md"))?;
-
-    Ok(())
-}
-
-#[test]
-fn generate_changelog_for_new_version() -> Result<()> {
-    let repository = create_repository().context("Failed to create repository")?;
-
-    let options = changelog_lib::Options {
-        repository_path: repository.dir.path(),
-        start_commit_id: INITIAL_COMMIT_ID,
-        repository: "andrzejressel/pulumi-gestalt",
-        changelog_dir: "tests/example/.changelog",
-    };
-
-    let result = changelog_lib::generate_changelog_for_new_version(&options, "0.3.0")?;
-
-    compare_with_file(&result, Path::new("tests/example/expected_new_version.md"))?;
-
-    Ok(())
-}
-
-#[test]
-fn generate_github_changelog_for_0_1_0() -> Result<()> {
-    let repository = create_repository().context("Failed to create repository")?;
-
-    let options = changelog_lib::Options {
-        repository_path: repository.dir.path(),
-        start_commit_id: INITIAL_COMMIT_ID,
-        repository: "andrzejressel/pulumi-gestalt",
-        changelog_dir: "tests/example/.changelog",
-    };
-
-    let result = changelog_lib::generate_changelog_for_github_changelog(&options, "0.1.0")?;
-
-    compare_with_file(&result, Path::new("tests/example/expected_github_0.1.0.md"))?;
-
-    Ok(())
-}
-
-#[test]
-fn generate_github_changelog_for_0_2_0() -> Result<()> {
-    let repository = create_repository().context("Failed to create repository")?;
-
-    let options = changelog_lib::Options {
-        repository_path: repository.dir.path(),
-        start_commit_id: INITIAL_COMMIT_ID,
-        repository: "andrzejressel/pulumi-gestalt",
-        changelog_dir: "tests/example/.changelog",
-    };
-
-    let result = changelog_lib::generate_changelog_for_github_changelog(&options, "0.2.0")?;
-
-    compare_with_file(&result, Path::new("tests/example/expected_github_0.2.0.md"))?;
-
-    Ok(())
-}
-
-#[test]
-fn generate_mkdocs_changelog() -> Result<()> {
-    let repository = create_repository().context("Failed to create repository")?;
-
-    let options = changelog_lib::Options {
-        repository_path: repository.dir.path(),
-        start_commit_id: INITIAL_COMMIT_ID,
-        repository: "andrzejressel/pulumi-gestalt",
-        changelog_dir: "tests/example/.changelog",
-    };
-
-    let result = changelog_lib::generate_mkdocs_changelog(&options)?;
-
-    compare_with_file(&result, Path::new("tests/example/expected_mkdocs.md"))?;
-
-    Ok(())
-}
-
-fn compare_with_file(content: &str, file: &Path) -> Result<()> {
+pub(crate) fn compare_with_file(content: &str, file: &Path) -> Result<()> {
     if replace_files() {
         fs::write(file, content)?;
     }
@@ -111,41 +19,12 @@ fn compare_with_file(content: &str, file: &Path) -> Result<()> {
     Ok(())
 }
 
-fn create_repository() -> Result<Repository> {
-    let repository = Repository::new()?;
-
-    let repository = repository
-        .copy_file("tests/example/.changelog/0.1.0/.gitkeep")?
-        .add_and_commit("Initial commit (MUST NOT BE INCLUDED)")?
-        .copy_file("tests/example/.changelog/0.1.0/1_added.yaml")?
-        .add_and_commit("Add 1_added.yaml")?
-        .copy_file("tests/example/.changelog/0.1.0/2_changed.yaml")?
-        .add_and_commit("Add 2_changed.yaml")?
-        .copy_file("tests/example/.changelog/0.1.0/3_deprecated.yaml")?
-        .add_and_commit("Add 3_deprecated.yaml")?
-        .copy_file("tests/example/.changelog/0.1.0/4_removed.yaml")?
-        .add_and_commit("Add 4_removed.yaml")?
-        .copy_file("tests/example/.changelog/0.1.0/5_fixed.yaml")?
-        .add_and_commit("Add 5_fixed.yaml")?
-        .copy_file("tests/example/.changelog/0.1.0/6_security.yaml")?
-        .add_and_commit("Add 6_security.yaml")?
-        .create_tag("v0.1.0")?
-        .add_and_commit("[no-changelog] Do not include in changelog")?
-        .add_and_commit("Some feature")?
-        .add_and_commit_renovate("Some renovate bot commit")?
-        .create_tag("v0.2.0")?
-        .copy_file("tests/example/.changelog/unreleased/1_added.yaml")?
-        .add_and_commit("Some yet unreleased feature")?;
-
-    Ok(repository)
-}
-
-struct Repository {
-    dir: TempDir,
+pub(crate) struct Repository {
+    pub(crate) dir: TempDir,
 }
 
 impl Repository {
-    fn new() -> Result<Self> {
+    pub(crate) fn new() -> Result<Self> {
         let temp_dir = tempdir().context("Failed to create temporary directory")?;
         std::process::Command::new("git")
             .arg("init")
@@ -155,7 +34,7 @@ impl Repository {
         Ok(Repository { dir: temp_dir })
     }
 
-    fn copy_file(self, file_name: &str) -> Result<Self> {
+    pub(crate) fn copy_file(self, file_name: &str) -> Result<Self> {
         let source = PathBuf::from(file_name);
         let destination = self.dir.path().join(file_name);
         let parent_dir = destination.parent().unwrap();
@@ -171,7 +50,7 @@ impl Repository {
         Ok(self)
     }
 
-    fn add_and_commit(self, message: &str) -> Result<Self> {
+    pub(crate) fn add_and_commit(self, message: &str) -> Result<Self> {
         let output = std::process::Command::new("git")
             .arg("add")
             .arg(".")
@@ -201,7 +80,7 @@ impl Repository {
         Ok(self)
     }
 
-    fn add_and_commit_renovate(self, message: &str) -> Result<Self> {
+    pub(crate) fn add_and_commit_renovate(self, message: &str) -> Result<Self> {
         let output = std::process::Command::new("git")
             .arg("add")
             .arg(".")
@@ -231,7 +110,7 @@ impl Repository {
         Ok(self)
     }
 
-    fn create_tag(self, tag_name: &str) -> Result<Self> {
+    pub(crate) fn create_tag(self, tag_name: &str) -> Result<Self> {
         let output = std::process::Command::new("git")
             .arg("tag")
             .arg(tag_name)
---
+++ changelog_lib/tests/integration_test_released.rs
@@ -0,0 +1,74 @@
+use crate::common::{compare_with_file, Repository};
+use anyhow::Context;
+use anyhow::Result;
+use std::path::Path;
+
+mod common;
+
+const INITIAL_COMMIT_ID: &str = "377a47ceaf4e362dd808c80d3e44ab7d0fe146e4";
+
+#[test]
+fn generate_changelog_test() -> Result<()> {
+    let repository = create_repository().context("Failed to create repository")?;
+
+    let options = changelog_lib::Options {
+        repository_path: repository.dir.path(),
+        start_commit_id: INITIAL_COMMIT_ID,
+        repository: "andrzejressel/pulumi-gestalt",
+        changelog_dir: "tests/example_released/.changelog",
+    };
+
+    let result = changelog_lib::generate_changelog(&options)?;
+
+    compare_with_file(&result, Path::new("tests/example_released/expected.md"))?;
+
+    Ok(())
+}
+
+#[test]
+fn generate_mkdocs_changelog() -> Result<()> {
+    let repository = create_repository().context("Failed to create repository")?;
+
+    let options = changelog_lib::Options {
+        repository_path: repository.dir.path(),
+        start_commit_id: INITIAL_COMMIT_ID,
+        repository: "andrzejressel/pulumi-gestalt",
+        changelog_dir: "tests/example_released/.changelog",
+    };
+
+    let result = changelog_lib::generate_mkdocs_changelog(&options)?;
+
+    compare_with_file(
+        &result,
+        Path::new("tests/example_released/expected_mkdocs.md"),
+    )?;
+
+    Ok(())
+}
+
+fn create_repository() -> Result<Repository> {
+    let repository = Repository::new()?;
+
+    let repository = repository
+        .copy_file("tests/example_released/.changelog/0.1.0/.gitkeep")?
+        .add_and_commit("Initial commit (MUST NOT BE INCLUDED)")?
+        .copy_file("tests/example_released/.changelog/0.1.0/1_added.yaml")?
+        .add_and_commit("Add 1_added.yaml")?
+        .copy_file("tests/example_released/.changelog/0.1.0/2_changed.yaml")?
+        .add_and_commit("Add 2_changed.yaml")?
+        .copy_file("tests/example_released/.changelog/0.1.0/3_deprecated.yaml")?
+        .add_and_commit("Add 3_deprecated.yaml")?
+        .copy_file("tests/example_released/.changelog/0.1.0/4_removed.yaml")?
+        .add_and_commit("Add 4_removed.yaml")?
+        .copy_file("tests/example_released/.changelog/0.1.0/5_fixed.yaml")?
+        .add_and_commit("Add 5_fixed.yaml")?
+        .copy_file("tests/example_released/.changelog/0.1.0/6_security.yaml")?
+        .add_and_commit("Add 6_security.yaml")?
+        .create_tag("v0.1.0")?
+        .add_and_commit("[no-changelog] Do not include in changelog")?
+        .add_and_commit("Some feature")?
+        .add_and_commit_renovate("Some renovate bot commit")?
+        .create_tag("v0.2.0")?;
+
+    Ok(repository)
+}
